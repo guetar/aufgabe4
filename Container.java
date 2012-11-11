4e6e372b056d3fc5fca214fee2ed62a9522f08d8
Matthias <Matthias@128.131.209.198>
2012-11-11 17:37:00 +0100

    Container erstellt

---
+++ Container.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author Matthias
+ */
+public abstract class Container<P> implements Pict{
+    
+    protected P[][] container;
+    private double scalefactor;
+    public Container(P[][] container, double scalefactor){
+    this.container=container;
+    this.scalefactor=scalefactor;
+    }
+    
+}
