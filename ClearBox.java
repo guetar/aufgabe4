af6d342ad540d1236e59fbfc5fd05e2653b2c97a
Matthias <Matthias@128.131.209.198>
2012-11-11 16:25:57 +0100

    ClearBox erstellt

---
+++ ClearBox.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Matthias
+ */
+public class ClearBox extends Printable{
+    
+    //VB: width>0; height>0
+    public ClearBox(double width, double height){
+        super('*',' ',width,height);   
+    }
+    
+    //NB: return width/height 
+    public double getAspectRatio(){
+    return getWidth()/getHeight();
+    }
+}
