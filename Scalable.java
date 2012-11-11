32003c57db5c9075647fd6ed039fccc523987126
Matthias <Matthias@128.131.209.198>
2012-11-11 15:45:36 +0100

    Scalable angelegt.

---
+++ Scalable.java
@@ -0,0 +1,23 @@
+
+/**
+ *
+ * @author Matthias
+ */
+public abstract class Scalable implements Pict {
+    //IV: width>0;height>0
+    private double width;
+    private double height;
+
+    //VB:width>0;height>0
+    //NB:this.width>0;this.height>0
+    public Scalable(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    //VB: 0.1 <= factor <= 10.0;
+    public void scale(double factor) {
+        width = width * factor;
+        height = height * factor;
+    }
+}
