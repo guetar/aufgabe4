bf5c74ee479545da9bdf476db8da26b431aa8572
gbernsteiner <guetar@gmx.at>
2012-11-11 16:23:30 +0100

    Added Freebox

---
+++ FreeBox.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author guetar
+ */
+public class FreeBox extends Scalable {
+    
+    private String input;
+    
+    public FreeBox(String input, int width, int height) {
+        super(width, height);
+        this.input = input;
+    }
+    
+    @Override
+    public String toString() {
+        String output = "";
+        return output;
+    }
+}
