c4e87b903b7d9643268f8b13ebce3e2dd1ecb2e8
Matthias <Matthias@128.131.209.198>
2012-11-11 17:57:46 +0100

    getter setter in Container

--- Container.java
+++ Container.java
@@ -6,10 +6,23 @@
 public abstract class Container<P> implements Pict{
     
     protected P[][] container;
-    private double scalefactor;
-    public Container(P[][] container, double scalefactor){
+    public Container(P[][] container){
     this.container=container;
-    this.scalefactor=scalefactor;
+    }
+    protected int getWidth(){
+    return container.length;
+    }
+    
+    protected int getHeight(){
+    return container[0].length;
+    }
+    
+    protected P getBox(int x, int y){
+    return container[x][y];
+    }
+    
+    protected void setBox(int x, int y,P box){
+    container[x][y]=box;
     }
     
 }
