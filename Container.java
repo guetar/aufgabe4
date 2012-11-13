c0813360939da7c2d5689155b4828f58079e7378
steff <steff@ono-sendai>
2012-11-13 15:35:11 +0100

    test

--- Container.java
+++ Container.java
@@ -3,26 +3,27 @@
  *
  * @author Matthias
  */
-public abstract class Container<P> implements Pict{
-    
+public abstract class Container<P> implements Pict {
+
     protected P[][] container;
-    public Container(P[][] container){
-    this.container=container;
+
+    public Container(P[][] container) {
+        this.container = container;
     }
-    protected int getWidth(){
-    return container.length;
+
+    protected int getWidth() {
+        return container.length;
     }
-    
-    protected int getHeight(){
-    return container[0].length;
+
+    protected int getHeight() {
+        return container[0].length;
     }
-    
-    protected P getBox(int x, int y){
-    return container[x][y];
+
+    protected P getBox(int x, int y) {
+        return container[x][y];
     }
-    
-    protected void setBox(int x, int y,P box){
-    container[x][y]=box;
+
+    protected void setBox(int x, int y, P box) {
+        container[x][y] = box;
     }
-    
 }
