3f8862def929a1c505c8e998c347df92bfdab025
Matthias <Matthias@128.131.209.198>
2012-11-13 14:39:26 +0100

    Kommis hinzugefügt

--- Container.java
+++ Container.java
@@ -3,26 +3,36 @@
  *
  * @author Matthias
  */
-public abstract class Container<P> implements Pict{
+public abstract class Container<P> implements Pict {
     
+    //IV: container!=null; container[0]!=null
     protected P[][] container;
-    public Container(P[][] container){
-    this.container=container;
+
+    //VB: container!=null; container[0]!=null
+    //NB: this.container!=null; this.container[0]!=null
+    public Container(P[][] container) {
+        this.container = container;
     }
-    protected int getWidth(){
-    return container.length;
+
+    //NB: return this.container.length
+    protected int getWidth() {
+        return container.length;
     }
-    
-    protected int getHeight(){
-    return container[0].length;
+
+    //NB: return this.container[0].length
+    protected int getHeight() {
+        return container[0].length;
     }
-    
-    protected P getBox(int x, int y){
-    return container[x][y];
+
+    //VB: x>=0;y>=0
+    //NB: return this.container[x][y]
+    protected P getBox(int x, int y) {
+        return container[x][y];
     }
-    
-    protected void setBox(int x, int y,P box){
-    container[x][y]=box;
+
+    //VB: x>=0;y>=0;box!=null
+    //NB: container[x][y]!=null
+    protected void setBox(int x, int y, P box) {
+        container[x][y] = box;
     }
-    
 }
