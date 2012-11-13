37fcaaf3b4a809587aba60b69c1bc260aefd2ec4
gbernsteiner <guetar@gmx.at>
2012-11-13 15:27:20 +0100

    Fixed setWidth setHeight Bug in Constructor

--- FreeBox.java
+++ FreeBox.java
@@ -7,8 +7,8 @@ public class FreeBox extends Scalable {
     private char[][] input;
     //VB: input!=NULL;width>0;height>0
     //NB: this.input=char[inputWidth][inputHeight];this.width>0;this.height>0
-    public FreeBox(String input, int width, int height) {
-        super(width, height);
+    public FreeBox(String input) {
+        super();
         
         String n = System.getProperty("line.separator");
         String[] inputLines = input.split(n);
