bcbb2be3786617fd6a7c2550dfcc1539fe3122fb
gbernsteiner <guetar@gmx.at>
2012-11-13 15:54:20 +0100

    Finished some Scaled

--- Scaled.java
+++ Scaled.java
@@ -15,8 +15,10 @@ public class Scaled<P extends Pict> extends Container {
     public void scale(double factor) {
         for(int i = 0; i < getWidth(); i++) {
             for(int j = 0; j < getHeight(); j++) {
+                
                 Scalable box = (Scalable) getBox(i, j);
                 box.scale(factor);
+                
                 if(box.getWidth() > maxWidth) maxWidth = box.getWidth();
                 if(box.getHeight() > maxHeight) maxHeight = box.getHeight();
             }
@@ -26,10 +28,25 @@ public class Scaled<P extends Pict> extends Container {
     @Override
     public String toString() {
         String output = "";
+        String delimiter = System.getProperty("line.separator");
+        
         for(int i = 0; i < getWidth(); i++) {
             for(int j = 0; j < getHeight(); j++) {
+                
                 P box = (P) getBox(i, j);
-                output += box.toString(j);
+                String[] boxOut = box.toString().split(delimiter);
+                
+                for(int k = 0; k < maxHeight; k++) {
+                    output += boxOut[k];
+
+                    int diff = (int) Math.round(maxWidth) - boxOut[k].length();
+                    if(diff > 0) {
+                        for(int l = 0; l < diff; l++) {
+                            output += " ";
+                        }
+                    }
+                    output += "\n";
+                }
             }
         }
         return output;
